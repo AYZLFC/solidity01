394a8517c4f6f3f76c54bffafa3270659727ed93
AYZLFC <almogzamir@gmail.com>
2023-01-08 15:48:46 +0200

    Simple bank 2

--- contracts/Simplebank.sol
+++ contracts/Simplebank.sol
@@ -2,18 +2,55 @@
 pragma solidity >=0.4.22 <0.9.0;
  
 contract Simplebank {
-    function addFunds() external payable{
-        funders.push(msg.sender);
-        fundersSumSent.push(msg.value);
+    uint public numberOfFunders;
+    mapping(address => bool) private funders;
+    mapping(uint => address) private lutFunders;
+    address public owner;
+
+    constructor (){
+        owner = msg.sender;
     }
 
-    address[] public funders;
-    uint[] fundersSumSent;
+    modifier onlyOwner(){
+        require(msg.sender == owner, "Only the owner can do that");
+        _;
+    }
+
+    function transferOwnership(address newOwner) external onlyOwner{
+        owner = newOwner;
+    }
+
+    function addFunds() external payable{
+        address funder = msg.sender;
+        if(!funders[funder]){
+            uint index = numberOfFunders++;
+            funders[funder] = true;
+            lutFunders[index] = funder;
+        }    
+    }
+
+    function getAllFunders() external view returns(address[] memory) {
+        address[] memory _funders = new address[](numberOfFunders);
+        for(uint i=0; i<numberOfFunders; i++){
+            _funders[i]=lutFunders[i];
+        }
+        return _funders;
+    }
+
+    function withdraw(uint withdrawAmount) external{
+        require(withdrawAmount < 1000000000000000000 || msg.sender == owner, "You can't withdraw more than 1 ether");
+        payable (msg.sender).transfer(withdrawAmount);
+    }
+
+    
 }
 
-
+// truffle migrate --reset
 // truffle console
 // const instance = await Simplebank.deployed()
 // instance.addFunds({value:"500000000000000000", from:accounts[0]})
 // instance.addFunds({value:"500000000000000000", from:accounts[1]})
 // const funds = instance.funds()
+// instance.getAllFunders()
+// instance.withdraw(1000000000000000000)
+// instance.transferOwnership("0x2f2568A97d71D36636f638a8d2Effb3987f0799E")
